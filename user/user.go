04c95205749f7b6023b9e5e6da93456dcf7789d7
0xmuralik <krishna192reddy@gmail.com>
2023-09-23 04:36:35 -0700

    create user

---
+++ user/user.go
@@ -0,0 +1,110 @@
+package user
+
+import (
+	"fmt"
+
+	"github.com/0xmuralik/file-share/database"
+	fiber "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/session"
+)
+
+var Store *session.Store
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func Home(ctx *fiber.Ctx) error {
+	sess, err := Store.Get(ctx)
+	if err != nil {
+		return fmt.Errorf("cannot get session: %v", err)
+	}
+
+	if auth := sess.Get("authenticated"); auth == nil || !auth.(bool) {
+		return ctx.Status(503).SendString("This is home, User is not logged in")
+	}
+
+	username := sess.Get("user")
+	return ctx.SendString(fmt.Sprintf("This is home, welcome %s", username))
+}
+
+func Register(ctx *fiber.Ctx) error {
+	db := database.UserDBConn
+
+	user := new(User)
+	if err := ctx.BodyParser(user); err != nil {
+		return ctx.Status(503).SendString(fmt.Sprintf("Cannot register user, error: %s", err.Error()))
+	}
+
+	res := db.Create(&user)
+	if res.Error != nil {
+		return ctx.Status(503).SendString(fmt.Sprintf("Cannot create user: %s", res.Error.Error()))
+	}
+	ctx.JSON(&user)
+	return ctx.Status(200).SendString("User created successfully")
+}
+
+func Delete(ctx *fiber.Ctx) error {
+	db := database.UserDBConn
+
+	sess, err := Store.Get(ctx)
+	if err != nil {
+		return fmt.Errorf("cannot get session: %v", err)
+	}
+
+	if auth := sess.Get("authenticated"); !auth.(bool) {
+		return ctx.Status(503).SendString("User is not logged in")
+	}
+
+	username := sess.Get("user")
+
+	var user User
+	db.First(&user, "username=?", username)
+	if user.Username == "" {
+		return ctx.Status(500).SendString("No file found")
+	}
+
+	db.Delete(&user)
+	ctx.Status(200).SendString("User deleted successfully")
+	return ctx.Redirect("/home")
+}
+
+func LogIn(ctx *fiber.Ctx) error {
+
+	db := database.UserDBConn
+	sess, err := Store.Get(ctx)
+	if err != nil {
+		return fmt.Errorf("cannot get session: %v", err)
+	}
+	req := new(User)
+	if err := ctx.BodyParser(req); err != nil {
+		return ctx.Status(503).SendString(fmt.Sprintf("Invalid req: %s", err.Error()))
+	}
+
+	var user User
+	db.Find(&user, "username=?", req.Username)
+
+	if user.Username != "" && user.Password == req.Password {
+		sess.Set("authenticated", true)
+		sess.Set("user", user.Username)
+		sess.Save()
+		ctx.SendString(fmt.Sprintf("User logged in successfully. Username: %s", user.Username))
+		return ctx.Redirect("/api/v1/user/home")
+	}
+
+	return ctx.Status(503).SendString("Invalid username or password")
+}
+
+func LogOut(ctx *fiber.Ctx) error {
+	sess, err := Store.Get(ctx)
+	if err != nil {
+		return fmt.Errorf("cannot get session: %v", err)
+	}
+
+	sess.Delete("authenticated")
+	sess.Save()
+	ctx.SendString("User logged out")
+
+	return ctx.Redirect("/api/v1/user/home")
+}

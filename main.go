ec4c01ec83bfebbf0a1f16d4ba1c461101942773
0xmuralik <krishna192reddy@gmail.com>
2023-09-25 01:44:17 -0700

    html pages

--- main.go
+++ main.go
@@ -2,27 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/0xmuralik/file-share/database"
 	"github.com/0xmuralik/file-share/file"
 	"github.com/0xmuralik/file-share/user"
 	fiber "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
 	"github.com/gofiber/fiber/v2/middleware/session"
 	"github.com/jinzhu/gorm"
 )
 
 func routes(app *fiber.App) {
-	api := app.Group("/api")
 
-	v1 := api.Group("/v1")
+	app.Get("/home", user.Home)
 
-	userAPI := v1.Group("/user")
+	userAPI := app.Group("/user")
 	userAPI.Post("/login", user.LogIn)
 	userAPI.Post("/logout", user.LogOut)
 	userAPI.Post("/register", user.Register)
-	userAPI.Get("/home", user.Home)
 
-	fileAPI := v1.Group("/file")
+	fileAPI := app.Group("/file")
 	fileAPI.Get("/id/:file_id", file.GetFileById)
 	fileAPI.Get("/name/:name", file.GetFileByName)
 	fileAPI.Get("/:owner", file.GetFiles)
@@ -56,6 +56,11 @@ func initDatabase() {
 
 func main() {
 	app := fiber.New()
+
+	app.Use(filesystem.New(filesystem.Config{
+		Root:   http.Dir("./templates"),
+		Browse: true,
+	}))
 
 	initDatabase()
 	defer database.FileDBConn.Close()
